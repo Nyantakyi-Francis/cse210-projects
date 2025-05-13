13938c1fcef248d8a71dce897f6bb7c6155c439c
Nyantakyi-Francis <nyantakyifrancis54@gmail.com@byupathway.edu>
2025-05-13 07:01:41 +0000

    week 02 journals

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine(entry.ToFileString());
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        entries.Clear();
+        foreach (var line in File.ReadAllLines(filename))
+        {
+            entries.Add(Entry.FromFileString(line));
+        }
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Save journal to file");
+            Console.WriteLine("4. Load journal from file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = promptGenerator.GetRandomPrompt();
+                    Console.WriteLine($"\nPrompt: {prompt}");
+                    Console.Write("Your response: ");
+                    string response = Console.ReadLine();
+                    string date = DateTime.Now.ToString("yyyy-MM-dd");
+                    journal.AddEntry(new Entry(date, prompt, response));
+                    break;
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter filename to save: ");
+                    journal.SaveToFile(Console.ReadLine());
+                    break;
+                case "4":
+                    Console.Write("Enter filename to load: ");
+                    journal.LoadFromFile(Console.ReadLine());
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
     }
 }
