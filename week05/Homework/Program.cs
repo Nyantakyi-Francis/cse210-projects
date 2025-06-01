3d6f9976ab252c9155042f52742e1aff21211795
Nyantakyi-Francis <nyantakyifrancis54@gmail.com@byupathway.edu>
2025-06-01 16:48:00 +0000

    week 05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test Assignment
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        // Test MathAssignment
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        // Test WritingAssignment
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
